f4e8aa9dd41465e392ec17935dda04a85b7e3508
Claudio Cesar Sanchez Tejeda <demonccc.y@gmail.com>
2026-09-21 02:12:11 -0300

    Export Attention Region node

--- ccc_krea2/modular_nodes/__init__.py
+++ ccc_krea2/modular_nodes/__init__.py
@@ -1,5 +1,6 @@
 """Package initialization for the split Krea2 Edit nodes."""
 
+from .attention_region_node import CcCKrea2AttentionRegion
 from .visual_reference_node import CcCKrea2VisualReference
 from .semantic_reference_node import CcCKrea2SemanticReference
 from .size_resolver_node import CcCKrea2SizeResolver
@@ -11,6 +12,7 @@ from .paint_prepare_node import CcCKrea2PaintPrepare
 from .paint_node import CcCKrea2Paint
 
 __all__ = [
+    "CcCKrea2AttentionRegion",
     "CcCKrea2VisualReference",
     "CcCKrea2SemanticReference",
     "CcCKrea2SizeResolver",
